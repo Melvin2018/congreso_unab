6cbdfce5907127ca356d0ef99ebffc9afd2fa98d
melvin <ro0211032016@unab.edu.sv>
2020-01-14 10:20:22 -0600

    dependencia

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,54 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer
+      v-model="drawer"
+      style="font-size:20px"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+    >
+      <v-list dense>
+        <v-list-item link href="/">
+          <v-list-item-icon>
+            <v-img
+              height="30px"
+              width="30px"
+              src="https://cdn4.iconfinder.com/data/icons/election-world-color/64/senate-congress-government-senator-political-512.png"
+            ></v-img>
+          </v-list-item-icon>
+          <v-list-item-title>Congreso</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      :clipped-left="$vuetify.breakpoint.lgAndUp"
+      app
+      dark
+      src="https://picsum.photos/1920/1080?random"
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(55,236,186,.7), rgba(25,32,72,.7)"
+        ></v-img>
+      </template>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        Action Goal
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <router-view />
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  data: () => ({
+    drawer: null
+  })
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,30 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
-Vue.config.productionTip = false
-
+import BootstrapVue from 'bootstrap-vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import firebase from 'firebase/app'
+import vuetify from '@/plugins/vuetify'
+Vue.prototype.$path = "http://192.168.43.16:8080"
+Vue.prototype.$axios = axios
+firebase.initializeApp({
+    apiKey: "AIzaSyDivyvXgYoDQnLX8j02ErCs3upsm6OiFKY",
+    authDomain: "action-gol.firebaseapp.com",
+    databaseURL: "https://action-gol.firebaseio.com",
+    projectId: "action-gol",
+    storageBucket: "action-gol.appspot.com",
+    messagingSenderId: "253574338725",
+    appId: "1:253574338725:web:883d607e79a6bb3263554c",
+    measurementId: "G-ES21S4TLB7"
+});
+Vue.use(VueAxios, axios)
+Vue.use(BootstrapVue)
+Vue.use(vuetify)
+Vue.config.productionTip = false;
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    vuetify,
+    render: h => h(App)
+}).$mount('#app');
